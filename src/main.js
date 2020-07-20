a4b0be46f212d377dca06c7b8edf9986d1e47c5b
Jiten Singh <jitenshahi@Jitens-Macbook-Pro.local>
2020-07-20 19:02:33 -0400

    basic routing configuration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { sync } from 'vuex-router-sync';
+import store from './store';
+import router from './route';
 
+sync(store, router);
 Vue.config.productionTip = false
 
 new Vue({
-    render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#root');
---
+++ src/route/routes.js
@@ -0,0 +1,8 @@
+import Home from '@/components/home/Index';
+
+export default [
+    {
+        path: '/',
+        component: Home
+    }
+];

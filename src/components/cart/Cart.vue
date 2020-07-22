cc9dc438483fc91579c5be007c152ca2a2d2f506
Jitendra Singh Shahi <jshahi0105@conestogac.on.ca>
2020-07-22 03:01:57 -0400

    added cart and forget password design

--- src/components/cart/Cart.vue
+++ src/components/cart/Cart.vue
@@ -1,13 +1,21 @@
 <template>
-    $END$
+	<a-drawer title="Cart" class="has-text-centered" :width="420" :visible="visible"
+		:body-style="{ paddingBottom: '80px' }" @close="onClose">
+		<h4 class="title is-5">
+			Cart Empty
+		</h4>
+	</a-drawer>
 </template>
-
 <script>
     export default {
-        name: 'Cart'
+        name: 'Cart',
+        props: {
+            visible: Boolean,
+        },
+        methods:{
+            onClose() {
+                this.$emit('close');
+            }
+        },
     }
 </script>
-
-<style scoped>
-
-</style>
--- src/components/user/ForgetPassword.vue
+++ src/components/user/ForgetPassword.vue
@@ -1,10 +1,59 @@
 <template>
-    $END$
+	<a-drawer title="Forgot Password" class="has-text-centered" width="420" :visible="visible"
+		:body-style="{ paddingBottom: '80px' }" @close="onClose">
+		<a-form :form="form" class="login-form" @submit="handleSubmit">
+			<a-form-item label="Email" class="has-text-left">
+				<a-input
+					v-decorator="[
+						'email',
+						{ rules: [
+							{ required: true, message: 'Please input your email!' },
+							{ type: 'email', message: 'Please input valid email!'}
+						] },
+					]"
+					placeholder="email">
+					<a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+				</a-input>
+			</a-form-item>
+			<a-form-item class="mt-3">
+				<div class="field is-grouped is-grouped-centered">
+					<p class="control">
+						<button type="submit" class="button has-text-white has-background-primary">
+							Submit
+						</button>
+					</p>
+				</div>
+			</a-form-item>
+		</a-form>
+	</a-drawer>
 </template>
 
 <script>
     export default {
-        name: 'ForgetPassword'
+        name: 'ForgetPassword',
+        props: {
+            visible: Boolean
+        },
+        data: () => {
+            return{
+            }
+        },
+        beforeCreate() {
+            this.form = this.$form.createForm(this, { name: 'forget_password_form' });
+        },
+        methods: {
+            onClose() {
+                this.$emit('close-forget-password');
+            },
+            handleSubmit(e) {
+                e.preventDefault();
+                this.form.validateFields((err, values) => {
+                    if (!err) {
+                        console.log('Received values of form: ', values);
+                    }
+                });
+            },
+        }
     }
 </script>
 
